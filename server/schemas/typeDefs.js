55fc1a6832031476588ac397b83bc001f433f98a
Brian King <bnk5532@gmail.com>
2022-10-25 12:55:53 -0400

    typeDefs and resolvers added

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,26 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    
+  }
+`;
+
+module.exports = typeDefs;
